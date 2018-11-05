16175cb6b47b7c73e5e8c16cd8a845e25a7ec928
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-05 10:18:50 -0800

    Update new_component.sh

--- new_component.sh
+++ new_component.sh
@@ -16,3 +16,22 @@ sed -i -e 's/MyComponent/'"$component"'/g' $TARGET/driver.js
 
 cp -f $DIR/data/generic_component.css $TARGET/style.css
 sed -i -e 's/MyComponent/'"$component"'/g' $TARGET/style.css
+
+#!/bin/bash
+
+#$1 == name of new component; $2 == filepath to inherit from.
+TARGET_NAME=$1
+TARGET=./src/components/$1
+INHERIT_FROM=$2
+SOURCE_NAME=$(basename $2)
+
+mkdir -p $TARGET
+
+cp -f ../$INHERIT_FROM/$SOURCE_NAME.html $TARGET/$TARGET_NAME.html
+
+cp -f ../$INHERIT_FROM/_$SOURCE_NAME.html $TARGET/_$TARGET_NAME.html
+
+cp -f ../$INHERIT_FROM/$SOURCE_NAME.js $TARGET/$TARGET_NAME.js
+
+cp -f ../$INHERIT_FROM/$SOURCE_NAME.css $TARGET/$TARGET_NAME.css
+
